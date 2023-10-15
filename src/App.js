065097cdefcaaa3d317d2b23bea0e99de779ecd8
Sarthak Verma <sarthakverma9645@gmail.com>
2023-10-14 23:15:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Nandita Khati</h1>
+        <p>3rd Year Student</p>
+        <a href="https://www.linkedin.com/in/nandita-khati-352511235/">LinkedIn</a>
+        <p>Contact : 8130079544</p>
+        <p>Email : nandita.khati2021@vitstudent.ac.in</p>
       </header>
     </div>
   );
